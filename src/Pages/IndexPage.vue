d4ad256835ea6931011fe86130f1b7c524ebb3e9
qianxuebing <you@example.com>
2018-07-12 13:46:57 +0800

    temp

--- src/Pages/IndexPage.vue
+++ src/Pages/IndexPage.vue
@@ -1,14 +1,126 @@
 <template>
-    <div class="content">content</div>
+    <div class="index-wrap">
+      <div class="index-left">
+          <div class="index-left-block">
+            <h2>全部产品</h2>
+            <template v-for="product in productList">
+              <h3>{{product.title}}</h3>
+              <ul>
+                <li v-for="item in product.list">
+                  <a :href="item.url">{{item.name}}</a>
+                  <span v-if="item.hot" class="hot-tag">HOT</span>
+                </li>
+              </ul>
+              <div v-if="!product.last" class="hr"></div>
+            </template>
+          </div>
+          <div class="index-left-block lastest-news">
+            <h2>最新消息</h2>
+            <ul>
+              <li v-for="item in newsList">
+                <a :href="item.url" class="new-item">{{item.title}}</a>
+              </li>
+            </ul>
+          </div>
+      </div>
+      <div class="index-right"></div>
+    </div>
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      productList: {
+        pc: {
+          title: "PC产品",
+          list: [
+            {
+              name: "数据统计",
+              url: "http://starcraft.com"
+            },
+            {
+              name: "数据预测",
+              url: "http://warcraft.com"
+            },
+            {
+              name: "流量分析",
+              url: "http://overwatch.com",
+              hot: true
+            },
+            {
+              name: "广告发布",
+              url: "http://hearstone.com"
+            }
+          ]
+        },
+        app: {
+          title: "手机应用类",
+          last: true,
+          list: [
+            {
+              name: "91助手",
+              url: "http://weixin.com"
+            },
+            {
+              name: "产品助手",
+              url: "http://twitter.com",
+              hot: true
+            },
+            {
+              name: "智能地图",
+              url: "http://maps.com"
+            },
+            {
+              name: "团队语音",
+              url: "http://phone.com"
+            }
+          ]
+        }
+      },
+      newsList: []
+    };
+  }
+};
 </script>
-<style lang="less">
-.content {
-  height: 40px;
-  background-color: blue;
-  color: #fff;
+<style lang="less" scoped>
+.index-wrap {
+  width: 1200px;
+  margin: 0 auto;
+  overflow: hidden;
+  display: flex;
+  display: -webkit-flex;
+  .index-left {
+    width: 300px;
+    text-align: left;
+    .index-left-block {
+      margin: 15px;
+      background-color: #fff;
+      box-shadow: 0 0 1px #ddd;
+      h2 {
+        padding: 10px 15px;
+        background: #4fc08d;
+        color: #fff;
+        margin-bottom: 20px;
+      }
+      h3 {
+        padding: 0 15px 5px 15px;
+        font-weight: bold;
+        color: #222;
+      }
+      ul {
+        padding: 10px 15px;
+        li {
+          padding: 5px;
+        }
+      }
+      .hr {
+        margin-bottom: 20px;
+      }
+    }
+  }
+  .index-right {
+    flex: 1;
+  }
 }
 </style>
 
