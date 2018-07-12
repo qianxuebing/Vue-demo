fe3f609dc43d1e27c0b5cc6c11f06f0d984b8585
qianxuebing <you@example.com>
2018-07-12 14:25:18 +0800

    right

--- src/Pages/IndexPage.vue
+++ src/Pages/IndexPage.vue
@@ -23,13 +23,27 @@
             </ul>
           </div>
       </div>
-      <div class="index-right"></div>
+      <div class="index-right">
+        <div class="index-board-list">
+          <div class="index-board-item" v-for="(item,index) in boardList" 
+          :class="[{'line-last':index%2!==0},'index-board'+item.id]">
+            <div class="index-board-item-inner">
+              <h2>{{item.title}}</h2>
+              <p>{{item.description}}</p>
+              <div class="index-board-button">
+                立即购买
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
 </template>
 <script>
 export default {
   data() {
     return {
+      newsList: [],
       productList: {
         pc: {
           title: "PC产品",
@@ -77,7 +91,36 @@ export default {
           ]
         }
       },
-      newsList: []
+      boardList: [
+        {
+          title: "开放产品",
+          description: "开放产品是一款开放产品",
+          id: "car",
+          toKey: "analysis",
+          saleout: false
+        },
+        {
+          title: "品牌营销",
+          description: "品牌营销帮助你的产品找到更好的定位",
+          id: "earth",
+          toKey: "count",
+          saleout: false
+        },
+        {
+          title: "使命必达",
+          description: "使命必达快速迭代永远保持最前端的速度",
+          id: "loud",
+          toKey: "forecast",
+          saleout: true
+        },
+        {
+          title: "勇攀高峰",
+          description: "帮你勇闯高峰，到达事业顶峰",
+          id: "hill",
+          toKey: "publish",
+          saleout: false
+        }
+      ]
     };
   }
 };
@@ -120,6 +163,18 @@ export default {
   }
   .index-right {
     flex: 1;
+    .index-board-list {
+      overflow: hidden;
+      display: flex;
+      display: -webkit-flex;
+      .index-board-item {
+        width: 400px;
+        background-color: #fff;
+        box-shadow: 0 0 1px #ddd;
+        padding: 20px;
+        margin-right: 20px;
+      }
+    }
   }
 }
 </style>
